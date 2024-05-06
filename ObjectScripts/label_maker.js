ee3937f7344d8381a31ad0f6e2a177374840429a
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-05-06 15:05:42 -0400

    update

--- ObjectScripts/label_maker.js
+++ ObjectScripts/label_maker.js
@@ -1,1 +1,15 @@
-function printContact(name, address, city, state, zipcode)
+function printContact(contact){
+    console.log(contact.name)
+    console.log(contact.address)
+    console.log(contact.city + ",", contact.state, contact.zip)
+}
+
+let myInfo = {
+    name: "Tiffany May", 
+    address: "121 Main Street",
+    city: "Lakeland",
+    state: "Florida",
+    zip: "33789",
+    /* other properties not shown */
+   };
+   printContact(myInfo);
